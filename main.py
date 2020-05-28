392b239967a4559e16280db9b31d28dab6aab5e3
mohitp3337 <mohitp3337@gmail.com>
2020-05-28 20:59:09 +0530

    User login and user registration functionality

--- main.py
+++ main.py
@@ -13,18 +13,21 @@ app.secret_key = 'loggedin'
 
 def db_connection():
     db = pymysql.connect(host=config['mysql_db']['host'],
-                        user = config['mysql_db']['user'],
-                        password = config['mysql_db']['password'],
-                        database = config['mysql_db']['database'])
-
-    #db = pymysql.connect("localhost", "root", "toor", "login_page")
-    #db = pymysql.connect("localhost", "root", "toor", "login_page")
+         user = config['mysql_db']['user'],
+         password = config['mysql_db']['password'],
+         database = config['mysql_db']['database'],
+         autocommit = config['mysql_db']['autocommit'])
     #cursor = db.cursor()
     cursor = db.cursor(pymysql.cursors.DictCursor)
-    #print("db >>>>>>>>>>>>")
-    #print(db)
     return cursor
 
+def connection():
+    conn = pymysql.connect(host=config['mysql_db']['host'],
+           user = config['mysql_db']['user'],
+           password = config['mysql_db']['password'],
+           database = config['mysql_db']['database'],
+           autocommit = config['mysql_db']['autocommit'])
+    return conn
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,86 +38,71 @@ def login():
         print("inside login >>>>>>>>>>>>")
         # Create variables for easy access
         username = request.form['username']
-        print("username >>>>>>>>>>>>>>>>>>>>>>>>")
-        print(username)
         password = request.form['password']
-        print("password >>>>>>>>>>>>>>>>>>>>>>>>")
-        print(password)
         # Check if account exists using MySQL
-        #cursor = db_connection()
-        #cursor.execute('SELECT * FROM user_credentials WHERE username = %s AND password = MD5(%s)', (username, password))
+        cursor = db_connection()
+        select_query = 'SELECT * FROM user_credentials WHERE username = %s AND password = MD5(%s)', (username, password)
+        cursor.execute(select_query)
         # Fetch one record and return result
-        #user_credentials = cursor.fetchone()
+        user_credentials = cursor.fetchone()
         # If account exists in accounts table in out database
-        #if user_credentials:
+        if user_credentials:
             # Create session data, we can access this data in other routes
-        #    session['loggedin'] = True
-        #    session['user_id'] = user_credentials['user_id']
-        #    session['username'] = user_credentials['username']
+            session['loggedin'] = True
+            session['username'] = user_credentials['username']
             # Redirect to home page
-        #    return 'Logged in successfully!'
-        #else:
+            return 'Logged in successfully!'
+        else:
             # Account doesnt exist or username/password incorrect
-        #    msg = 'Incorrect username/password!'
+            #msg = 'Incorrect username/password!'
+            return 'Incorrect username/password!'
     return render_template('login.html', msg='')
 
 
-# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
 #@app.route('/register', methods=['GET', 'POST'])
 @app.route('/register', methods=['POST'])
 def register():
     # Output message if something goes wrong...
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    #if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-    if request.method == 'POST' and 'name' in request.form and 'phone' in request.form and 'email' in request.form:
+    # Check if "username", "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         print("inside register >>>>>>>>>>>>>>>")
-        userid = request.form['userid']
         # Create variables for easy access
-        username = request.form['name']
-        print("Username - " + username)
-        phone = request.form['phone']
-        print("phone - " + phone)
-        email = request.form['email']
-        print("email - " + email)
+        username = request.form['username']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
         password = request.form['password']
-        conf_password = request.form['conf_password']
+        #conf_password = request.form['conf_password']
+        email = request.form['email']
+        cursor = db_connection()
+        # Check if account exists using MySQL
+        #cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user_credentials WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            print("inside else condition >>>>>>>>>>>>>>.")
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO user_credentials VALUES (%s, %s, %s, MD5(%s), %s)',
+                          (username, firstname, lastname, password, email))
+            conn = connection()
+            conn.commit()
+            msg = 'You have successfully registered!'
+
     elif request.method == 'POST':
         # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
     # Show registration form with message (if any)
-
-
-    cursor = db_connection()
-    # Check if account exists using MySQL
-    #cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM user_credentials WHERE username = %s', (username,))
-    account = cursor.fetchone()
-    # If account exists show error and validation checks
-    if account:
-        msg = 'Account already exists!'
-    elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-        msg = 'Invalid email address!'
-    elif not re.match(r'[A-Za-z0-9]+', username):
-        msg = 'Username must contain only characters and numbers!'
-    elif not username or not password or not email:
-        msg = 'Please fill out the form!'
-    else:
-        print("inside else condition >>>>>>>>>>>>>>.")
-        # Account doesnt exists and the form data is valid, now insert new account into accounts table
-        #cursor.execute('INSERT INTO user_credentials VALUES (NULL, %s, %s, %s)', (username, password, email,))
-        cursor.execute('INSERT INTO user_credentials VALUES (%s, %s, %s, %s)', (userid, username, password, email,))
-        mysql.connection.commit()
-        msg = 'You have successfully registered!'
-
-
-
-
     return render_template('login.html', msg=msg)
     #return render_template('mohit.html', msg=msg)
-
-
-
 
 @app.route('/administration', methods=['POST', 'GET'])
 def administration():
